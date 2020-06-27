ca63f990769b939ebe79abb99e764efecf15c8d1
gaurav.pandey1 <gaurav.pandey1@globallogic.com>
2020-06-27 21:21:04 +0530

    Rest End Points Created and Rest Calls Happening

--- Microservices - Communication and Service Discovery/movie-info-service/src/main/java/com/learn/movieinfoservice/resources/MovieResources.java
+++ Microservices - Communication and Service Discovery/movie-info-service/src/main/java/com/learn/movieinfoservice/resources/MovieResources.java
@@ -1,4 +1,16 @@
 package com.learn.movieinfoservice.resources;
 
+import com.learn.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieResources {
+
+    @RequestMapping("/{movieId}")
+    public Movie getResouces(@PathVariable("movieId") Integer movieId){
+        return new Movie(movieId,"Test Name.");
+    }
 }
